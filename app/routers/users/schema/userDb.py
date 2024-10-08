7275d366168d0a10ed101583c7d2855283603984
jair efren <jairromo54545@gmail.com>
2024-10-07 22:46:40 -0600

    something

--- app/routers/users/schema/userDb.py
+++ app/routers/users/schema/userDb.py
@@ -11,11 +11,11 @@ class UserSchema(Base):
     last_name_f = Column(String(15), nullable=False)
     last_name_m = Column(String(15), nullable=False)
     registration_date = Column(Date, nullable=False)
-    user_type = Column(SmallInteger, ForeignKey('usertype.user_type_id'), nullable=False)
+    user_type = Column(SmallInteger, ForeignKey('Usertype.user_type_id'), nullable=False)
     usertype = relationship("UserType", back_populates="users")
 
 class UserType(Base):
     __tablename__ = 'railway.Usertype'
     user_type_id = Column(SmallInteger, primary_key=True, index=True)
     type_name = Column(String(15), nullable=False)
-    users = relationship("UserSchema", back_populates="usertype")
+    users = relationship("UserSchema", back_populates="Usertype")
